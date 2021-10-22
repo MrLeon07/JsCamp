e7b42ff810481fe231fbb81578c23368217c6b30
MrLeon07 <ulgerguray1@gmail.com>
2021-10-22 11:52:14 +0300

    Ders sonu

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+let konutKrediler = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel konut kredileri"];
+
+console.log("<ul>")
+for (let index = 0; index < konutKrediler.length; index++) {
+    const element = konutKrediler[index];
+    console.log("<li>"+element+"</li>")
+    
+}
+console.log("</ul>")
+
+//Arrays
+
+
+
+//
